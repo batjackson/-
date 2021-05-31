f19507903e5f27d64dbc8e5286747ca76823c966
pppp-k <1035649196@qq.com>
2021-05-31 18:09:50 +0800

    添加数据库文件

---
+++ phrfile/sql文件/im.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 8.0.22 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `im` (
+	`id` int ,
+	`name` varchar ,
+	`pict1` varchar ,
+	`pict2` varchar ,
+	`pict3` varchar ,
+	`imfor` varchar 
+); 
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('1','三禾北京稻香村传统特产墨子酥美食点心零食','1.1.jpg','1.2.jpg','1.3.jpg','220g4块装墨子酥北京稻香村');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('2','旺旺雪饼仙贝500g年货置办零食小吃饼干','2.1.jpg','2.2.jpg','2.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('3','8盒 桂林特产桂花糕正宗绿豆糕传统手工老','3.1.jpg','3.2.jpg','3.3.jpg','过节送礼，好实惠，送礼袋');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('4','上海特产老字号沈大成双酿团芝麻豆沙团传统','4.1.jpg','4.2.jpg','4.3.jpg','一个团子两种馅料豆沙和芝麻');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('5','老式鸡蛋糕传统手工老蛋糕糕点零食早餐小吃','5.1.jpg','5.2.jpg','5.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('6','好丽友派巧克力派早餐面包提拉米苏蛋糕抹茶','6.1.jpg','6.2.jpg','6.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('7','山东特产蜜三刀正宗手工麻球油蜜食金果子老','7.1.jpg','7.2.jpg','7.3.jpg','蜜三刀 蜜痳条 蜜翻花 传统老式糕点');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('8','榎屋日式照烧酱汁丸子串日本传统美食糕点好','8.1.jpg','8.2.jpg','8.3.jpg','照烧酱汁味 好吃不贵');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('9','法思觅语 冻干草莓果干芒果榴莲果脯即食休','9.1.jpg','9.2.jpg','9.3.jpg','巧克力图层 水果东干 香甜可口');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('10','桂花糕传统手工正宗桂花米糕甜点小吃早餐网','10.1.jpg','10.2.jpg','10.3.jpg','满口留香 回味无穷 250g/包');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('11','天津特产槽子糕炉元老式传统糕点怀旧手工点','11.1.jpg','11.2.jpg','11.3.jpg','传统老味，外皮焦黄，扎实口感');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('12','孕妇孕期中晚期小吃山药薄片脆锅巴薯片健康','12.1.jpg','12.2.jpg','12.3.jpg','孕妇儿童健康零食 休闲小吃 充饥解馋好吃');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('13','五谷杂粮饭15谷杂粮米 孕妇 儿童 杂粮','13.1.jpg','13.2.jpg','13.3.jpg','低糖 杂粮饭15谷混合米 孕妇 儿童 健康美味');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('14','蒸烩煮冷冻炒饭料理包微波加热速食饭快餐包','14.1.jpg','14.2.jpg','14.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('15','回味赞 原味酸辣藤椒方便速食品夜宵米线南','15.1.jpg','15.2.jpg','15.3.jpg','鸭肝鸭肠鸭血+豆腐果+辣椒油，真材实料！');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('16','厨小吉10味冷冻盖浇饭外卖料理包商用快餐','16.1.jpg','16.2.jpg','16.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('17','【6桶装】小样自热米饭 煲仔饭方便速食米','17.1.jpg','17.2.jpg','17.3.jpg','好味道必须是组合装一起整！');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('18','外卖菜肴包快餐料理包速食小碗菜煲仔饭速冻','18.1.jpg','18.2.jpg','18.3.jpg',NULL);
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('19','小汤君自热米饭大份量12盒自助速食方便米','19.1.jpg','19.2.jpg','19.3.jpg','不用火不用电15分钟吃热饭加班户外旅行');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('20','五芳斋八宝饭390g*6特产小吃甜豆沙糯','20.1.jpg','20.2.jpg','20.3.jpg','家常八宝饭 软糯香甜 内含坚果');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('21','0脂即食魔芋凉皮零低代餐热量宽面速食方便','21.1.jpg','21.2.jpg','21.3.jpg','0脂肪');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('22','千石谷自热冲泡米饭18袋泡饭纯饭速食即食','22.1.jpg','22.2.jpg','22.3.jpg','送专用饭盒 自热款、冲泡款');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('23','麦小呆手撕9号素肉大豆蛋白豆干辣条零食小','23.1.jpg','23.2.jpg','23.3.jpg','特价9.9元素肉');
+insert into `im` (`id`, `name`, `pict1`, `pict2`, `pict3`, `imfor`) values('24','韩国直发不倒翁速食拌饭盖饭汤饭24种全增','24.1.jpg','24.2.jpg','24.3.jpg',NULL);
